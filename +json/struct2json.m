c31aee2c7e5dc53b58131b1f67e3a1d61f65a753
Azim J <57653434+azimj@users.noreply.github.com>
2020-10-23 16:31:47 -0600

    Update STRUCT2JSON
    * added error handling if file could not be opened for writing
    * added line breaks for each JSON token
    * To Do
    *     [ ] Figure out how to format line indentation

--- +json/struct2json.m
+++ +json/struct2json.m
@@ -21,9 +21,19 @@ regex_string='([,{}])';
 replacement_string='$1\n';
 ss=regexprep(ss,regex_string,replacement_string);
 
+% remove new-line between close braces and comma '}\n,' -> '},'
+regex_string_2 = '}\n,';
+replacement_string_2 = '},';
+ss=regexprep(ss,regex_string_2,replacement_string_2);
 %% Write data
 % open and write data to a file
-fid=fopen(json_file,'wb');
-cln=onCleanup(@()fclose(fid));
+try
+    fid=fopen(json_file,'wb');
+    cln=onCleanup(@()fclose(fid));
+catch
+    error('json2struct:OpenForWriteFailed', ...
+        "Could not open file: " + json_file + " for writing.");
+end
+
 
 fwrite(fid,ss);
