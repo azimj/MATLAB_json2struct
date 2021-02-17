b6af988ad32db09afaf40fa8803d52c2c7f70439
Azim J <57653434+azimj@users.noreply.github.com>
2021-02-17 09:33:25 -0700

    [feature] specify sub-struct
    Allow optionally extract a named element from JSON file

--- +json/json2struct.m
+++ +json/json2struct.m
@@ -1,7 +1,10 @@
-function json_struct=json2struct(file_name)
-% S=JSON2STRUCT(FILE_NAME)
+function json_struct=json2struct(file_name,varargin)
+% S=JSON.JSON2STRUCT(FILE_NAME,ELEMENT_NAME)
 %   Reads a JSON encoded file and returns a struct representing the JSON
-%   objects.
+%   objects and returns the specified element `element_name`
+%
+% S=JSON.JSON2STRUCT(FILE_NAME) Return all contents of the JSON file
+%
 %
 % See also:
 %   jsondecode
@@ -9,11 +12,16 @@ function json_struct=json2struct(file_name)
 % Azim Jinha 
 % 2020-09-13
 
+% Change Log
+% 2021-02-17
+%   * Added optional parameter to extract a sub-structre
+
 
 % Parse input and check that file exists
 p=inputParser;
 p.addRequired('file_name',@(f)exist(f,'file')==2);
-p.parse(file_name);
+p.addOptional('element_name','',@(s)isstring(s) || ischar(s));
+p.parse(file_name,varargin{:});
 
 fid=fopen(file_name);
 cln=onCleanup(@()fclose(fid));
@@ -23,5 +31,9 @@ try
      json_struct = jsondecode(file_contents);
 catch 
      error('json2struct:InvalidJSONdata',"File: '" + file_name +"' does not contain valid JSON");
+end
+
+if isfield(json_struct,p.Results.element_name)
+    json_struct = json_struct.(p.Results.element_name);
 end
 
