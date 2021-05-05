5f0491d13f5daf373f96a7911097284a477dfa64
Azim J <57653434+azimj@users.noreply.github.com>
2021-05-05 00:26:15 -0600

    Update for R2021b
    Use new pretty-print option in MATLAB r2021a jsonencode.
    Added missing parse to `json.struct2json`.

--- +json/struct2json.m
+++ +json/struct2json.m
@@ -1,12 +1,12 @@
 function struct2json(data_struct, json_file)
 % STRUCT2JSON(DATA,JSON_FILE)
-%   Saves data to a JSON file. 
+%   Saves data to a JSON file.
 %
-%  Warning: function uses `jsonencode` which does not guarantee array 
-%  shapes are preserved. A 1xn vector will be encoded as a nx1 and does 
+%  Warning: function uses `jsonencode` which does not guarantee array
+%  shapes are preserved. A 1xn vector will be encoded as a nx1 and does
 %  not guarantee preservationo  of data types.
 %
-% See also: 
+% See also:
 %   jsonencode
 
 % Azim Jinha
@@ -16,28 +16,42 @@ function struct2json(data_struct, json_file)
 p=inputParser;
 p.addRequired('data_struct',@isstruct);
 p.addRequired('json_file',@(c)isstring(c)||ischar(c));
-
+p.parse(data_struct,json_file);
 %% Convert data to JSON
-ss=jsonencode(data_struct,'ConvertInfAndNaN',false);
 
-% add a line break after commas and braces
-regex_string='(\[\{\}\]|)';
-replacement_string='$1\n';
-ss=regexprep(ss,regex_string,replacement_string);
-ss=regexprep(ss,',"',',\n"');
-% remove new-line between close braces and comma '}\n,' -> '},'
-regex_string_2 = '}\n,';
-replacement_string_2 = '},';
-ss=regexprep(ss,regex_string_2,replacement_string_2);
+if verLessThan('matlab','9')
+    json_string=prettyprintjson(data_struct);
+else
+    json_string=jsonencode(data_struct,ConvertInfAndNaN=false,PrettyPrint=true);
+end
+
 %% Write data
 % open and write data to a file
 try
     fid=fopen(json_file,'wb');
     cln=onCleanup(@()fclose(fid));
+    fwrite(fid,json_string);
 catch
     error('json2struct:OpenForWriteFailed', ...
         "Could not open file: " + json_file + " for writing.");
 end
 
 
-fwrite(fid,ss);
+
+
+end
+
+function pretty_json=prettyprintjson(data_struct)
+%PRETTY_JSON=PRETTYPRINTJSON(DATA_STRUCT) add line breaks to encoded JSON
+%JSON string
+json_string=jsonencode(data_struct,'ConvertInfAndNaN',false);
+% add a line break after commas and braces
+regex_string='(\[\{\}\]|)';
+replacement_string='$1\n';
+json_string=regexprep(json_string,regex_string,replacement_string);
+json_string=regexprep(json_string,',"',',\n"');
+% remove new-line between close braces and comma '}\n,' -> '},'
+regex_string_2 = '}\n,';
+replacement_string_2 = '},';
+pretty_json=regexprep(json_string,regex_string_2,replacement_string_2);
+end
--- Example.m
+++ Example.m
@@ -5,12 +5,17 @@
 
     Azim J
     2020-10-16
+
+    Change log
+    2021-05-04
+    * added second element to test struct
 %}
 %% Write `struct` to a JSON file
 s.Width=100;
 s.Height=200;
 s.Files.Name='test.json';
-s.Array=[1:10];
+s.Array=1:10;
+s(2)=s;
 json.struct2json(s,'test.json');
 
 %% Read JSON file
